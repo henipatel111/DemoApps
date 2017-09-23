e2fabf5e37f284c61608216ff83e02812ea4022c
henipatel <henipatel8913@gmail.com>
2017-09-23 16:46:36 +0530

    first commit

---
+++ app/src/main/java/com/example/heni/coordinatorlayoutexample/CustomMoveUpBehavior.java
@@ -0,0 +1,16 @@
+package com.example.heni.coordinatorlayoutexample;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+
+/**
+ * Created by heni on 18/9/17.
+ */
+
+public class CustomMoveUpBehavior extends CoordinatorLayout.Behavior {
+
+    public CustomMoveUpBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/com/example/heni/coordinatorlayoutexample/TrialActivity.java
@@ -0,0 +1,17 @@
+package com.example.heni.coordinatorlayoutexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by heni on 18/9/17.
+ */
+
+public class TrialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trial);
+    }
+}
